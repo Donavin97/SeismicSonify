4a89804a8d74bef51e701ef43d417a8e834b3d7b
seismo <Donavin.Liebgott@gmail.com>
2021-06-03 14:48:20 +0200

    Added audio dynamic compression to sonification code to make some events more audable. Output files are now modified according to compression values.

--- sonify.py
+++ sonify.py
@@ -4,12 +4,12 @@ from obspy import UTCDateTime
 sonify(
     network='GT',
     station='LBTB',
-    channel='BHZ',
-    starttime=UTCDateTime(2020, 7, 7, 10, 00),
-    endtime=UTCDateTime(2020, 7, 7, 12, 00),
+    channel='SHZ',
+    starttime=UTCDateTime(2021, 6, 3, 2, 30),
+    endtime=UTCDateTime(2021, 6, 3, 4, 00),
     freqmin=1,
     freqmax=23,
-    speed_up_factor=200,
+    speed_up_factor=100,
     fps=1,  # Use fps=60 to fully recreate the JHEPC entry (slow to save!)
     spec_win_dur=8,
     db_lim=(-180, -130),
--- sonify/sonify.py
+++ sonify/sonify.py
@@ -378,6 +378,8 @@ def _ffmpeg_combine(audio_filename, video_filename, output_filename):
         '320k',
         '-ac',
         '2',
+        '-filter_complex',
+        'compand=attacks=1:points=-120/-900|-90/-12|-60/-9|0/-7|20/-7:gain=5',
         output_filename,
     ]
     print('Combining video and audio using ffmpeg...')
